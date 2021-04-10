aea4c9a65f8e53c5d3cbe411661be02674496efc
Jacek <mrwp450@gmail.com>
2021-04-10 11:53:23 +0200

    added Donation saving feature

--- src/main/java/pl/sda/service/DonationService.java
+++ src/main/java/pl/sda/service/DonationService.java
@@ -3,6 +3,7 @@ package pl.sda.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import pl.sda.exception.WhiproundNotFoundException;
 import pl.sda.model.Donation;
 import pl.sda.model.Whipround;
 import pl.sda.model.dto.DonationDto;
@@ -26,7 +27,8 @@ public class DonationService {
     }
 
     private Whipround getWhiproundFromDBById(Long id) {
-        return whiproundRepository.findById(id).orElseThrow();
+        return whiproundRepository.findById(id).orElseThrow(()
+                -> new WhiproundNotFoundException("Whipround with following id not found: " + id));
     }
 
 }
